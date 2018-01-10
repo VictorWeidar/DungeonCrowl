bf4ca7a1521513e77010c9b56a49146cc443a599
Victor Weidar <Victor.weidar@gmail.com>
2018-01-10 14:01:02 +1000

    converted to unity

---
+++ Dungeon Crowl/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed;
+    public GameObject Model;
+    Camera Maincam;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        Maincam = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float xMov = Input.GetAxis("Horizontal");
+        float yMov = Input.GetAxis("Vertical");
+
+        float MoveX = xMov * speed * Time.deltaTime;
+        float MoveY = yMov * speed * Time.deltaTime;
+
+        transform.Translate(MoveX, 0, MoveY);
+	}
+}
